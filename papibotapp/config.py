8a89ab0a5d3f60aec23b9dfba083c0dfcf89d100
Arnaud Anselmi <Arnaud.Anselmi@tomra.com>
2020-03-09 18:10:51 +0100

    Proc !

--- papibotapp/config.py
+++ papibotapp/config.py
@@ -1,9 +1,9 @@
 import os
 
-if os.environ.get('GMAPS_API_KEY') is None:
+if os.environ.get('GOOGLE_API_KEY') is None:
     with open(os.path.join(os.path.dirname(__file__), "gmap"), "r") as api_key_local:
-        GMAPS_API_KEY = api_key_local.read()
+        GOOGLE_API_KEY = api_key_local.read()
 else:
-    GMAPS_API_KEY = os.environ.get('GMAPS_API_KEY')
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 
 SECRET_KEY = "sd,jmlfkjqxdv;,kmsjf05614812.06886"
